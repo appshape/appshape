08275702d9df93c75c768935be2725b0c0574c83
Patryk Wojtyński <patryk.wojtynski@elpassion.pl>
2015-10-11 23:13:14 +0200

    Admin in organizations instead of owner

--- app/services/organization/personal_organization_creator.rb
+++ app/services/organization/personal_organization_creator.rb
@@ -1,7 +1,5 @@
 class Organization::PersonalOrganizationCreator < Organization::OrganizationCreator
   def execute
-    Organization::PersonalOrganization.create(personal_owner: @user, name: @name).tap do |organization|
-      OrganizationUser.create(user: @user, organization: organization, role: :owner)
-    end
+    Organization::PersonalOrganization.create(personal_owner: @user, name: @name).tap { |organization| add_admin(organization) }
   end
 end
