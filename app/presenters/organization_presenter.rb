fc88dadb265df518c87eb2be3d0a6e1106d3107c
Patryk Wojtyński <patryk.wojtynski@gmail.com>
2015-11-01 21:16:36 +0100

    Organization users

--- app/presenters/organization_presenter.rb
+++ app/presenters/organization_presenter.rb
@@ -1,8 +1,16 @@
 class OrganizationPresenter
   attr_reader :organization
-  delegate :name, to: :organization
+  delegate :name, :users, to: :organization
 
   def initialize(organization)
     @organization = organization
+  end
+
+  def user_role(user)
+    organization_users.select {|ou| ou.user_id == user.id}.first.role
+  end
+
+  def organization_users
+    @organization_users ||= organization.organization_users
   end
 end
