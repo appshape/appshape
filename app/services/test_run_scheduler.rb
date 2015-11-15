24bc0e11c791810d63454f79b0243996fcb03fbb
aserafin <adrian@softmad.pl>
2015-11-15 17:13:25 +0100

    fixes for scheduling tasks

--- app/services/test_run_scheduler.rb
+++ app/services/test_run_scheduler.rb
@@ -1,7 +1,7 @@
 class TestRunScheduler
   def execute
-    tests.each do |test|
-      test['locations'].map do |location|
+    tests.values.each do |test|
+      test['locations'].each do |location|
         beanstalk.tubes.find("tests-#{location}-queue").put(test.to_json)
       end
     end
@@ -9,10 +9,17 @@ class TestRunScheduler
 
   private
   def tests
-    @tests ||= Rails.cache.read_multi(TestsForRunQuery.new.execute.map { |test| "test-#{test['id']}" })
+    @tests ||= begin
+      ids = TestsForRunQuery.new.execute.map { |test| "test-#{test.first}" }
+      ids.empty? ? [] : Rails.cache.read_multi(ids)
+    end
   end
 
   def beanstalk
     @beanstalk ||= Beaneater.new(Beaneater.configuration.beanstalkd_url)
+  end
+
+  def test_cache_key(id)
+    "test-#{id}"
   end
 end
