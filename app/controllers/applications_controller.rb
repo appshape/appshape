a18a8ee1c2d04078715b88ee0308cf748a08e2d3
aserafin <adrian@softmad.pl>
2015-12-13 18:59:26 +0100

    left navigation bar

--- app/controllers/applications_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationsController < ApplicationController
-  def index
-
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'applications#index'
+  root 'dashboard#index'
 end
