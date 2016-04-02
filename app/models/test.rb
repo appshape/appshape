a78b3b4fc098c549754a0e7f039fa5acb6475d1d
Patryk Wojtyński <patryk.wojtynski@gmail.com>
2016-04-02 02:09:27 +0200

    Update AMS, clean cache for destroyed test, delete test_runs after test_run_group destroy, fetch cached tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,8 +5,10 @@ class Test < ActiveRecord::Base
   has_one :request, autosave: true
   belongs_to :project
   has_one :organization, through: :project
+  has_many :test_run_groups, dependent: :destroy
 
   after_save :cache!
+  before_destroy :delete_cache!
 
   def cache_key
     "test-#{self.id}"
@@ -15,5 +17,9 @@ class Test < ActiveRecord::Base
   private
   def cache!
     Rails.cache.write(cache_key, TestFormatter.new(self).as_json)
+  end
+
+  def delete_cache!
+    Rails.cache.delete(cache_key)
   end
 end
